5b7f2c8953990e4208abe68aa470b3f8b9143af7
skad27 <adam.pezz@hotmail.com>
2024-07-02 18:00:22 +0200

    esplorazione e business goal

--- esplorazione_dataset.py
+++ esplorazione_dataset.py
@@ -8,3 +8,14 @@ Created on Tue Jul  2 16:18:41 2024
 import pandas as pd
 df = pd.read_csv(r'OnlineNewsPopularity.csv')
 
+print(df.info())
+print(df.head())
+print(df.describe())
+
+
+"""
+Business Goal: Predirre la popolarità mediatica di 
+vari articoli di Mashable,
+prendendo in considerazione diverse metriche derivanti da
+una indagine statistica.
+"""
